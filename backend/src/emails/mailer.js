83291302548f858e6e991b78d92fcda0102bf464
MapateLoum <loumpapamapate@gmail.com>
2026-04-13 17:24:19 +0000

    nodemail vers brevo

--- backend/src/emails/mailer.js
+++ backend/src/emails/mailer.js
@@ -1,9 +1,8 @@
-const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: parseInt(process.env.MAIL_PORT),
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
